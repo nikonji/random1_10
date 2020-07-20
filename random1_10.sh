50836785e7732699070a4565677982a2c372b0b1
Aymen Selmi <nikonselfmade@gmail.com>
2020-07-20 18:36:58 +0200

    A commit

---
+++ random1_10.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+###############
+
+echo ""
+echo "Welcome to Adjust_Task1"
+echo 'This is a script that writes the numbers from 1 - 10 in random order.'
+echo ""
+
+
+shuf -i 1-10
+
+
+echo ""
+echo "Enjoy your day!"
+echo ""
